b639fe349f72cf6147de9c4fa0a225b75c9eaff4
ehallam <elliothallam@gmail.com>
2024-08-07 16:34:01 +0100

    Initialised the spring boot application

---
+++ src/main/java/com/sky/SkyBankBackend/SkyBankBackendApplication.java
@@ -0,0 +1,13 @@
+package com.sky.SkyBankBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyBankBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyBankBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sky/SkyBankBackend/SkyBankBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sky.SkyBankBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkyBankBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
