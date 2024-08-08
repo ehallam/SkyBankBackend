4cc5326147c84cb96fa5d950747b1ff2f16699f3
ehallam <elliothallam@gmail.com>
2024-08-08 16:25:35 +0100

    fixed interface issues

--- src/main/java/com/sky/SkyBankBackend/services/CustomerService.java
+++ src/main/java/com/sky/SkyBankBackend/services/CustomerService.java
@@ -1,4 +1,19 @@
 package com.sky.SkyBankBackend.services;
 
-public class CustomerService {
+import com.sky.SkyBankBackend.DTO.CustomerDTO;
+import com.sky.SkyBankBackend.entities.Customer;
+import com.sky.SkyBankBackend.exceptions.CustomerNotFoundException;
+
+import java.util.List;
+
+public interface CustomerService {
+    CustomerDTO addCustomer(CustomerDTO newCustomer);
+
+    public CustomerDTO getCustomer(int id);
+
+    public List<CustomerDTO> getAll();
+
+    public CustomerDTO getCustomerByEmail(String email);
+
+    public CustomerDTO remove(int id);
 }
